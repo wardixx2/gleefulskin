4ba4518add030b4c1b8dde162683f4debdd1c79c
pangs <aguilarraphael79@gmail.com>
2026-06-09 13:48:55 +0800

    Fix GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ export default function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route
           path="/"
